1dcc03bc0604e137805ff923945b0cd354bb5571
iieo <achtbyte01@gmail.com>
2023-10-24 23:24:46 +0200

    added firebase

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,16 @@ import 'package:boilerplate/di/service_locator.dart';
 import 'package:boilerplate/presentation/my_app.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await setPreferredOrientations();
   await ServiceLocator.configureDependencies();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
 }
 
